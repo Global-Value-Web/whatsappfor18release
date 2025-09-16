032807f9f8f710b5b78c5dd22d00abe681f114d7
ShubhamBhawsar-GVW <shubham.b@global-value-web.com>
2025-09-16 16:14:01 +0530

    Update whatsappfor18release.py

--- whatsappfor18release.py
+++ whatsappfor18release.py
@@ -37,6 +37,7 @@ app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
+#mongo DB access
 MONGO_URI = "mongodb://192.168.1.100:27017/"
 client = MongoClient(MONGO_URI)
 db = client['PatientSafe']
